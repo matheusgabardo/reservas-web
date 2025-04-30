f9014af8ac6cdd0f5ac9d049025aea6b139e68ee
Matheus Messias <matheusgabardo677@gmail.com>
2025-04-30 20:33:59 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import AuthLayout from "./pages/layouts/auth";
+import Login from "./pages/auth/login/Index";
+import Register from "./pages/auth/register/Index";
+import DashboardLayout from "./pages/layouts/dashboard";
+import Home from "./pages/dashboard/home/Index";
+import Reservations from "./pages/dashboard/reservations/Index";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="auth" element={<AuthLayout />}>
+            <Route index path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+          <Route path="dashboard" element={<DashboardLayout />}>
+            <Route index element={<Home />} />
+            <Route path="reservations" element={<Reservations />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>,
+)
---
+++ src/pages/dashboard/reservations/Index.jsx
@@ -0,0 +1,7 @@
+export default function Reservations() {
+    return (
+        <>
+            <h1>Login</h1>
+        </>
+    );
+}
